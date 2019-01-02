4e4b5ac3e51086c5ec6a1aace4fbd8f391ee9abc
zbloom4 <zach@pocketpoints.com>
2019-01-01 17:35:52 -0800

    Adding React

---
+++ resources/js/components/lead-form.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+export default class LeadForm extends React.Component {
+
+    constructor(props, context) {
+        super(props, context);
+
+        this.state = {};
+    }
+
+    render() {
+
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
+
+
---
+++ resources/js/webpack.config.js
@@ -0,0 +1,58 @@
+var path = require('path');
+var webpack = require('webpack');
+var WebpackBuildNotifierPlugin = require('webpack-build-notifier');
+const devMode = process.env.NODE_ENV !== 'production';
+
+module.exports = {
+    entry: [__dirname + '/app.js'],
+    output: { path: __dirname + "../../../public/js/dist", filename: 'app.bundle.js' },
+    module: {
+        rules: [
+            {
+                test: /.jsx?$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/,
+                query: {
+                    presets: ['es2015', 'react']
+                },
+            },
+
+            {
+                test: /\.(png|jpe?g|gif|svg)$/,
+                use: [
+                    {
+                        loader: "file-loader",
+                        options: {
+                            name: "[name].[ext]",
+                            outputPath: "../../images/",
+                            emitFile: true
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.(eot|ttf|woff?.)$/,
+                use: [
+                    {
+                        loader: "file-loader",
+                        options: {
+                            name: "[name].[ext]",
+                            outputPath: "../../fonts/",
+                            emitFile: true
+                        }
+                    }
+                ]
+            }
+        ],
+    },
+    plugins: [
+        new WebpackBuildNotifierPlugin({
+            title: "My Project Webpack Build",
+            logo: path.resolve("./img/favicon.png"),
+            suppressSuccess: false,
+            sound: false
+        }),
+    ],
+    devtool: '#eval-source-map',
+    mode: "development"
+};
